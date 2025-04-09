46d023aeccc658851665454919a3c7e4d7588499
ludndev <ludndev@gmail.com>
2025-04-09 22:03:08 +0100

    feat(web): add 404 error page for unmatched routes

---
+++ web/src/views/NotFoundView.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="not-found">
+    <h1>404</h1>
+    <p>Page not found</p>
+  </div>
+</template>
+
+<style>
+    @media (min-width: 1024px) {
+        .not-found  {
+            min-height: 100vh;
+            display: flex;
+            align-items: center;
+            align-content: center;
+            justify-content: center;
+            flex-direction: column;
+        }
+    }
+</style>
+  
